ed8f681e179a1030510ef6d0a1b1d0391cc25f87
DwikiAnggara <dwiki.github@gmail.com>
2020-07-12 12:47:04 +0800

    Project Uas Mobile

---
+++ lib/app_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  final Color colorPrimary = Colors.blue;
+  final Color colorSecondary = Colors.amber;
+  final Color colorTertiary = Colors.redAccent;
+}
